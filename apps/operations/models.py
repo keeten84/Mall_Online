ce9d367eb346e4e098afcf2e5454c9b957b4387c
keeten84 <58552964@qq.com>
2018-10-11 13:31:43 +0800

    complete models of operations

--- apps/operations/models.py
+++ apps/operations/models.py
@@ -1,3 +1,65 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from goods.models import Goods
+
+User = get_user_model()
+
+
+class UserFav(models.Model):
+    '''用户收藏'''
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods,verbose_name='商品',on_delete=models.CASCADE)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '用户收藏'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.user.name
+
+class UserLeavingMessage(models.Model):
+    '''用户留言'''
+    MESSAGE_CHOICES = (
+        (1,'留言'),
+        (2,'投诉'),
+        (3,'询问'),
+        (4,'售后'),
+        (5,'求购'),
+    )
+
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+    message_type = models.CharField(default=1,max_length=10,choices=MESSAGE_CHOICES,verbose_name='用户留言类型',
+                                    help_text='用户留言:1(留言)，2(投诉)，3(询问)，4(售后)，5(求购)')
+    subject = models.CharField(default='',max_length=100,verbose_name='留言主题')
+    message = models.TextField(default='',max_length=300,verbose_name='留言内容',help_text='留言内容')
+    file = models.FileField(verbose_name='上传文件',help_text='上传的文件')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '用户留言'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.subject
+
+
+
+class UserAddress(models.Model):
+    '''用户收货地址'''
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+    district = models.CharField(default='',max_length=100,verbose_name='区域')
+    address = models.CharField(default='',max_length=100,verbose_name='详细地址')
+    signer_name = models.CharField(default='',max_length=100,verbose_name='收件人姓名')
+    signer_mobile = models.CharField(default='',max_length=11,verbose_name='收件人手机')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '用户收货地址'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.address
